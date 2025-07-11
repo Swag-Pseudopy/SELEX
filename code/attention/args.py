d3067263ca09985721da6976ad35b275684e86b2
Swagato Das <76675264+Swag-Pseudopy@users.noreply.github.com>
2025-07-11 10:47:21 +0200

    Update args.py
    added a few arguments for the changes made in the attention code.

--- code/attention/args.py
+++ code/attention/args.py
@@ -48,12 +48,22 @@ def get_arg_parser():
                         help='Proportion of the dataset to use for validation')
     parser.add_argument('--test_prop', type=float, default=0.1,
                         help='Proportion of the dataset to use for testing')
-    parser.add_argument('--num_layers', type=int, default=4, #1
+    parser.add_argument('--num_layers', type=int, default=1, #4
                         help='Number of layers in the model')
     parser.add_argument('--num_cross_att', type=int, default=1, #2
                         help='Number of cross-attention layers in the model')
-    parser.add_argument('--plot_base', type=str, default='scratch/plots_KL_norm', #'plots_RCA'
+    parser.add_argument('--plot_base', type=str, default='scratch/plots_flow3', #'plots_MSE', #'plots_KL_norm'#'plots_RCA'
                         help='Path to save/load the torch dataset')
-    parser.add_argument('--model_dir', type=str, default='scratch/model_KL_norm', #'model_RCA'
+    parser.add_argument('--model_dir', type=str, default='scratch/model_flow3',  #'model_MSE', #'model_KL_norm', #'model_RCA'
                         help='Path to save/load the torch dataset')
+    parser.add_argument('--loss', type=str, choices=['MSE', 'KL'], default='MSE',
+                        help='Loss function to use: "MSE" for Mean Squared Error or "KL" for Kullback-Leibler divergence (default: KL)')
+    parser.add_argument('--round_embedding_dim', type=int, default=16,
+                        help='Round number for embedding dimension')
+    parser.add_argument('--n_freqs', type=int, default=8,
+                        help='Number of frequency bands for positional encoding')
+    parser.add_argument('--num_tren', type=int, default=1,
+                        help='Number of layers of transformer encoder')
+    parser.add_argument('--mid_layer_abundance_injection', action='store_true', default=True,
+                        help='Whether to inject abundance information at the mid layer')
     return parser
