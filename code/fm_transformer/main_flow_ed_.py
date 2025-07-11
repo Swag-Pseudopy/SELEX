6e681bd46a9c773af2768c29e498f80bf73593ec
Swagato Das <76675264+Swag-Pseudopy@users.noreply.github.com>
2025-07-11 10:55:04 +0200

    Update main_flow_ed_.py

--- code/fm_transformer/main_flow_ed_.py
+++ code/fm_transformer/main_flow_ed_.py
@@ -25,8 +25,8 @@ global_start_time = time.time()
 # from rich import box
 
 # from emb import *
-from emb_ed import *
-# from emb_tr_ed import *
+# from emb_ed import *
+from emb_tr_ed import *
 
 from fmm import *
 
@@ -282,9 +282,6 @@ for split_idx, (train_files, val_files) in enumerate(splits):
         # with Live(generate_layout(), refresh_per_second=4, screen=False) as live:
         with tqdm(train_dataloader, desc=f"Split {split_idx+1} Epoch {epoch+1}/{num_epochs} [Train]", leave=not False) as pbar:
             for batch, file_indices, rho, rho_next in pbar:
-                if batch.shape[0] != batch_size:
-                    print(f"Skipping batch with shape {batch.shape} (expected {batch_size})")
-                    continue
                 batch_start_time = time.time()
                 filename = flat_dataset.keys[file_indices[0].item()]
                 if filename == excluded_file:
@@ -296,8 +293,7 @@ for split_idx, (train_files, val_files) in enumerate(splits):
                 batch = batch.to(device)
                 # print(batch[:5])
 
-                with torch.no_grad():
-                    embeddings = embedding_model(batch)
+                embeddings = embedding_model(batch, tgt=batch, teacher_forcing=True)
                 pos_emb = pos_embedding_layer(batch)
                 final_embedding = embeddings + pos_emb
 
@@ -306,13 +302,12 @@ for split_idx, (train_files, val_files) in enumerate(splits):
                 candidate_indices = torch.where(torch.tensor(flat_dataset.file_indices) == next_file_idx)[0]
                 # print(len(candidate_indices))
                 sampled_indices = candidate_indices[torch.randint(0, len(candidate_indices), (batch_size,))]
-                next_file_batch = torch.stack(flat_dataset.data).to(device)
-                next_file_batch = next_file_batch[sampled_indices]
+                next_file_batch = torch.stack(flat_dataset.data)
+                next_file_batch = next_file_batch[sampled_indices].to(device)
 
                 # print(next_file_batch[:5])
 
-                with torch.no_grad():
-                    embeddings_tgt = embedding_model(next_file_batch)
+                embeddings_tgt = embedding_model(next_file_batch, tgt=next_file_batch, teacher_forcing=True)
                 pos_emb_tgt = pos_embedding_layer(next_file_batch)
                 final_embedding_tgt = embeddings_tgt + pos_emb_tgt
 
@@ -346,7 +341,7 @@ for split_idx, (train_files, val_files) in enumerate(splits):
                 # print(loss, y_pred.shape, y_true.shape)
 
                 # Absolute relative error
-                abs_rel_error = torch.median(torch.abs((y_pred - y_true) / (y_true + 1e-8)))
+                abs_rel_error = torch.abs((y_pred - y_true) / (y_true + 1e-8))
                 train_abs_rel_errors.append(abs_rel_error.detach().cpu())
 
                 # Log of the loss
@@ -388,13 +383,12 @@ for split_idx, (train_files, val_files) in enumerate(splits):
                         continue
                     if batch.shape[0] != batch_size:
                         print(f"Skipping batch with shape {batch.shape} (expected {batch_size})")
-                        continue                    
+                        continue
 
                     batch = batch.to(device)
                     # print(batch[:5])
 
-                    with torch.no_grad():
-                        embeddings = embedding_model(batch)
+                    embeddings = embedding_model(batch, tgt=batch, teacher_forcing=True)
                     pos_emb = pos_embedding_layer(batch)
                     final_embedding = embeddings + pos_emb
 
@@ -403,13 +397,12 @@ for split_idx, (train_files, val_files) in enumerate(splits):
                     candidate_indices = torch.where(torch.tensor(flat_dataset.file_indices) == next_file_idx)[0]
                     # print(len(candidate_indices))
                     sampled_indices = candidate_indices[torch.randint(0, len(candidate_indices), (batch_size,))]
-                    next_file_batch = torch.stack(flat_dataset.data).to(device)
-                    next_file_batch = next_file_batch[sampled_indices]
+                    next_file_batch = torch.stack(flat_dataset.data)
+                    next_file_batch = next_file_batch[sampled_indices].to(device)
 
                     # print(next_file_batch[:5])
 
-                    with torch.no_grad():
-                        embeddings_tgt = embedding_model(next_file_batch)
+                    embeddings_tgt = embedding_model(next_file_batch, tgt=next_file_batch, teacher_forcing=True)
                     pos_emb_tgt = pos_embedding_layer(next_file_batch)
                     final_embedding_tgt = embeddings_tgt + pos_emb_tgt
 
@@ -610,8 +603,7 @@ try:
         next_file_batch = torch.tensor(flat_dataset.data[torch.randperm
                                     (batch_size,torch.tensor(flat_dataset.file_indices)==
                                                             next_file_idx)], device = device)
-        with torch.no_grad():
-            embeddings_tgt = embedding_model(next_file_batch)
+        embeddings_tgt = embedding_model(next_file_batch, tgt=next_file_batch, teacher_forcing=True)
         pos_emb_tgt = pos_embedding_layer(next_file_batch)
         final_embedding_tgt = embeddings_tgt + pos_emb_tgt
 
@@ -641,13 +633,16 @@ baseline_errors = []
 with torch.no_grad():
     for batch, file_indices, rho, rho_next in tqdm(test_dataloader, desc="Testing"):
         filename = flat_dataset.keys[file_indices[0].item()]
+        if filename == excluded_file:
+            continue
+
         batch = batch.to(device)
+        # print(batch[:5])
         if batch.shape[0] != batch_size:
             print(f"Skipping batch with shape {batch.shape} (expected {batch_size})")
             continue        
-        # print(batch[:5])
-        with torch.no_grad():
-            embeddings = embedding_model(batch)
+
+        embeddings = embedding_model(batch, tgt=batch, teacher_forcing=True)
         pos_emb = pos_embedding_layer(batch)
         final_embedding = embeddings + pos_emb
 
@@ -656,12 +651,12 @@ with torch.no_grad():
         candidate_indices = torch.where(torch.tensor(flat_dataset.file_indices) == next_file_idx)[0]
         # print(len(candidate_indices))
         sampled_indices = candidate_indices[torch.randint(0, len(candidate_indices), (batch_size,))]
-        next_file_batch = torch.stack(flat_dataset.data).to(device)
-        next_file_batch = next_file_batch[sampled_indices]
+        next_file_batch = torch.stack(flat_dataset.data)
+        next_file_batch = next_file_batch[sampled_indices].to(device)
 
         # print(next_file_batch[:5])
-        with torch.no_grad():
-            embeddings_tgt = embedding_model(next_file_batch)
+
+        embeddings_tgt = embedding_model(next_file_batch, tgt=next_file_batch, teacher_forcing=True)
         pos_emb_tgt = pos_embedding_layer(next_file_batch)
         final_embedding_tgt = embeddings_tgt + pos_emb_tgt
 
